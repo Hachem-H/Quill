1f43b64cb8a3a6db202456e5b89f05f3ab80790c
Hachem-H <hh.naram@gmail.com>
2023-11-02 19:25:40 +0200

    Setup for windowing

--- src/main.cc
+++ src/main.cc
@@ -1,51 +1,22 @@
-#include <opencv2/opencv.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include <GLFW/glfw3.h>
 
-#include <iostream>
+#define WINDOW_WIDTH  1920
+#define WINDOW_HEIGHT 1080
 
-static int vectorizationLevel = 128;
-static cv::Mat vectorizedImage;
-
-void UpdateVectorizedImage(int, void* data)
+int main()
 {
-    cv::Mat& grayscaleImage = *(cv::Mat*) data;
-    cv::threshold(grayscaleImage, vectorizedImage, vectorizationLevel, 255, cv::THRESH_BINARY);
-    cv::imshow("Vectorized Image", vectorizedImage);
-}
+    glfwInit();
 
-int main(void) {
-    cv::Mat image = cv::imread("Tests/Text Test #1.png");
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT,
+                                          "Quill", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
 
-    if (image.empty()) {
-        std::cerr << "[ERR]: Could not open file.\n";
-        return -1;
-    }
-
-    cv::Mat grayscaleImage;
-    cv::cvtColor(image, grayscaleImage, cv::COLOR_BGR2GRAY);
-
-    vectorizedImage = grayscaleImage.clone();
-
-    cv::namedWindow("Vectorized Image", cv::WINDOW_NORMAL);
-    cv::imshow("Vectorized Image", vectorizedImage);
-
-    cv::createTrackbar("Vectorization Level", "Vectorized Image", &vectorizationLevel, 255, UpdateVectorizedImage, &grayscaleImage);
-
-    for (bool isRunning = true; isRunning;) 
+    while (!glfwWindowShouldClose(window))
     {
-        switch (cv::waitKey(0))
-        {
-        case 14:
-        {
-            cv::imwrite("output.png", vectorizedImage);
-            std::cout << "Vectorized image saved as vectorized_image.png" << std::endl;
-            isRunning = false;
-        } break;
-
-        case 13:
-        {
-            isRunning = false;
-        } break;
-        }
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
